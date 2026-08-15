51a6a3c8a98f4460afe16a8c566ed9e28a59e7f3
Dimash Bakytzhanov <dimash.bakytzhanov@gmail.com>
2026-08-15 14:47:13 +0500

    fix: gitignore fix

--- .design-sync/previews/Spinner.tsx
+++
@@ -1,9 +0,0 @@
-import { Spinner } from 'quizlec-front';
-
-export function Default() {
-  return (
-    <div style={{ background: '#f7f9fc', minHeight: 120 }}>
-      <Spinner />
-    </div>
-  );
-}
